a3617e0be04024bcb21b7766eb250641cefeab82
derickx1 <derickwxie@gmail.com>
2019-03-20 14:59:11 -0700

    Added FreeFall

---
+++ Assets/Scripts/GoodWarp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodWarp : MonoBehaviour {
+
+	private GameObject Player;
+	private Player PlayerScript;
+
+	// Use this for initialization
+	void Awake () {
+		Player = GameObject.Find("Player");
+		PlayerScript = Player.GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PlayerScript.IsTriggering(this.gameObject))
+		{
+			Player.transform.position = new Vector3 (0, 150, 80);
+		}
+	}
+}
