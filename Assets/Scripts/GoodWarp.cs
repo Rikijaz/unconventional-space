90e90d52b73743e59f62b66a2dbae80c12502fa2
derickx1 <derickwxie@gmail.com>
2019-03-20 15:43:32 -0700

    Modified FreeFall

--- Assets/Scripts/GoodWarp.cs
+++ Assets/Scripts/GoodWarp.cs
@@ -6,6 +6,7 @@ public class GoodWarp : MonoBehaviour {
 
 	private GameObject Player;
 	private Player PlayerScript;
+	private bool Hit = false;
 
 	// Use this for initialization
 	void Awake () {
@@ -17,7 +18,12 @@ public class GoodWarp : MonoBehaviour {
 	void Update () {
 		if (PlayerScript.IsTriggering(this.gameObject))
 		{
+			Hit = true;	
+		}
+		if (Hit && Player.transform.position.y < -90)
+		{
 			Player.transform.position = new Vector3 (0, 150, 80);
+			Hit = false;
 		}
 	}
 }
